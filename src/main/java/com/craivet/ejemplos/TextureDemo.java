23e6b9beb3990af1ea2097c4774ca0aa6cf25e49
Juan Debenedetti <juandebenedetti94@gmail.com>
2023-12-08 18:38:38 -0300

    Update

--- src/main/java/com/craivet/ejemplos/TextureDemo.java
+++ src/main/java/com/craivet/ejemplos/TextureDemo.java
@@ -16,7 +16,7 @@ import static org.lwjgl.opengl.GL11.*;
 import static com.craivet.Global.*;
 
 /**
- * <h2>Sistema de Coordenadas en LWJGL</h2>
+ * <h2>Sistema de Coordenadas</h2>
  * En LWJGL (y en muchos otros sistemas graficos), el origen del sistema de coordenadas se encuentra en la esquina superior
  * izquierda de la pantalla o area de visualizacion. Esto significa que la coordenada (0, 0) corresponde a la esquina superior
  * izquierda de la ventana, y las coordenadas aumentan hacia abajo y hacia la derecha.
@@ -25,47 +25,37 @@ import static com.craivet.Global.*;
  * verticalmente de arriba hacia abajo. Por lo tanto, en LWJGL, el eje x positivo se extiende hacia la derecha desde el origen, y
  * el eje y positivo se extiende hacia abajo desde el origen.
  * <br><br>
- * <h2>Proyeccion Ortografica</h2>
- * La proyeccion ortografica es un tipo de transformacion utilizada en graficos por computadora y en el campo de la vision
- * tridimensional para mapear un objeto tridimensional en un plano bidimensional sin tener en cuenta la perspectiva. En otras
- * palabras, la proyeccion ortografica representa los objetos con proporciones iguales, independientemente de su distancia desde
- * la camara. Esta tecnica es diferente de la proyeccion perspectiva, que simula la forma en que los objetos se ven en la vida
- * real, donde los objetos mas cercanos parecen mas grandes que los objetos mas lejanos.
- * <p>
- * En la proyeccion ortografica, los rayos de luz que viajan desde los vertices del objeto hacia la camara son paralelos. Este
- * enfoque simplificado es util en situaciones donde no se necesita o no se desea un efecto de perspectiva, como en graficos 2D o
- * en ciertos estilos de juegos.
- * <p>
- * En OpenGL, una proyeccion ortografica se logra comunmente utilizando la funcion glOrtho(). Al configurar adecuadamente los
- * parametros de glOrtho(), puedes especificar el volumen de vision ortografica y proyectar los objetos en un espacio
- * bidimensional sin tener en cuenta la profundidad. Este tipo de proyeccion es util en aplicaciones como interfaces de usuario,
- * mapas 2D y otros casos donde la percepcion de la profundidad no es crítica.
- * <p>
- * En resumen, la proyeccion ortografica es un metodo de proyeccion que simplifica la representacion de objetos tridimensionales
- * al proyectarlos en un plano bidimensional con proporciones iguales, sin tener en cuenta la perspectiva.
- * <p>
- * La llamada a <b><i>glOrtho(left, right, bottom, top, zNear, zFar)</i></b> en OpenGL, como mencionado anteriormente, establece
- * una matriz de proyeccion ortografica. Esta configuracion en particular define un volumen de vision ortografica en el espacio
- * tridimensional. La interpretacion detallada de los parametros es la siguiente:
- * <p>
- * <b>left y right:</b> Establecen las coordenadas x del plano izquierdo y derecho del volumen de vision.
- * <p>
- * <b>bottom y top:</b> Establecen las coordenadas y del plano inferior y superior del volumen de vision.
- * <p>
- * <b>zNear y zFar:</b> Representan las distancias del plano cercano y lejano respecto a la camara.
- * <p>
- * Dado que glOrtho define una proyeccion ortografica, todos los objetos dentro de este volumen de vision ortografica seran
- * proyectados en la pantalla sin efectos de perspectiva. La llamada glOrtho() configura una matriz de proyeccion ortografica
- * adecuada para un sistema de coordenadas en el que la esquina superior izquierda de la pantalla es el origen (0,0) y las
- * coordenadas x aumentan hacia la derecha, mientras que las coordenadas y aumentan hacia abajo.
+ * <h2>Proyeccion en perspectiva</h2>
+ *
  * <br><br>
- * <h2>Matriz</h2>
- * Una matriz se refiere a un objeto que representa una matriz matematica. En el contexto de graficos 3D y OpenGL, las matrices se
- * utilizan para realizar transformaciones en los objetos tridimensionales, como rotaciones, traslaciones y escalas.
+ * <h2>Proyeccion Ortogonal</h2>
+ * En OpenGL, la proyeccion ortogonal se refiere a la transformacion que se aplica a las coordenadas tridimensionales de una
+ * escena para proyectarlas en un plano bidimensional de manera ortogonal. Esto significa que las lineas de proyeccion son
+ * perpendiculares al plano de proyeccion. La proyeccion ortogonal es comúnmente utilizada en situaciones donde se desea preservar
+ * las proporciones y tamaños relativos de los objetos en la escena, independientemente de su profundidad en el espacio
+ * tridimensional.
+ * <p>
+ * En OpenGL, hay una matriz de proyeccion que se utiliza para aplicar la proyeccion. Para establecer una proyeccion ortogonal en
+ * OpenGL, generalmente se usa la funcion <b>glOrtho</b>. Esta funcion toma como parametros los limites izquierdo, derecho, 
+ * inferior, superior, cerca y lejano del volumen de vista ortogonal. Los objetos fuera de este volumen no seran visibles en la 
+ * proyeccion.
+ * <p>
+ * Aqui hay un ejemplo simple en el que se establece una proyeccion ortogonal en OpenGL:
+ * <pre>{@code
+ * glMatrixMode(GL_PROJECTION);
+ * glLoadIdentity();
+ * glOrtho(left, right, bottom, top, near, far);
+ * glMatrixMode(GL_MODELVIEW);
+ * }</pre>
+ * Donde left, right, bottom, top, near, y far son los parametros que definen el volumen de vista ortogonal.
+ * <p>
+ * Despues de establecer la proyeccion ortogonal, los objetos en la escena se renderizaran proyectados en el plano bidimensional
+ * según la configuracion de la matriz de proyeccion. Esta matriz se combina con otras transformaciones de modelo y vista para
+ * finalmente generar las coordenadas de pantalla y renderizar la escena en la ventana de OpenGL.
  * <br><br>
  * <h3>Modos de matrices</h3>
  * Los modos de matrices en OpenGL se utilizan para especificar sobre que matriz se realizaran las operaciones subsiguientes.
- * Cada uno de estos modos se refiere a un tipo específico de matriz y su funcion en el pipeline grafico.
+ * Cada uno de estos modos se refiere a un tipo especifico de matriz y su funcion en el pipeline grafico.
  * <ul>
  * <li>GL_MODELVIEW: Este modo se utiliza para realizar operaciones de transformacion de modelo y vista. Es el modo predeterminado
  * al iniciar una aplicacion OpenGL.
